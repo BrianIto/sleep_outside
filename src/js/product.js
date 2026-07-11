e5d852614af12d5bc48819bef011660623bd961f
Brian Moura <brian.oliveira100@gmail.com>
2026-07-11 13:45:08 -0400

    remove unnecesary console log

--- src/js/product.js
+++ src/js/product.js
@@ -3,7 +3,6 @@ import ProductData from "./ProductData.mjs";
 import ProductDetails from "./ProductDetails.mjs";
 import { getParam } from "./utils.mjs";
 
-console.log("hello world");
 const productId = getParam("product");
 const dataSource = new ProductData("tents");
 
