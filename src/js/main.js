8f5142c7a8c3d52c19819c14f97ed217a6177036
Brian Moura <brian.oliveira100@gmail.com>
2026-07-13 11:49:58 -0400

    fix: mapping

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,6 @@
 import ProductList from "./ProductList.mjs";
 import ProductData from "./ProductData.mjs";
-
+import { loadTemplate, renderWithTemplate } from "./utils.mjs";
 const dataSource = new ProductData("tents");
 const productList = new ProductList(
   dataSource,
@@ -9,3 +9,11 @@ const productList = new ProductList(
 );
 
 productList.init();
+
+const headerTemplate = await loadTemplate("../partials/header.html");
+const headerElement = document.querySelector("header");
+renderWithTemplate(headerTemplate, headerElement);
+
+const footerTemplate = await loadTemplate("../partials/footer.html");
+const footerElement = document.querySelector("footer");
+renderWithTemplate(footerTemplate, footerElement);
